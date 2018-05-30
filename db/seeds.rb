48beedafbc52a0730b9fe6d1cd9d2d86e059ef64
Minh <minhdc@atmarkcafe.org>
2018-05-30 17:20:32 +0700

    set config mail for devise forgot password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create email: 'ogremoon54@gmail.com', firstname: 'minh', lastname: 'dao', password: '123456'
